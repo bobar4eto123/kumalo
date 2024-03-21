140dba842631f5e31b567ba25b9753adec1f846e
CootsAndLoons <cootsgrebesloons@gmail.com>
2024-03-21 21:56:04 +0200

    Validation for EditAccount.cshtml

--- kumalo/kumalo/Models/AccountModel.cs
+++ kumalo/kumalo/Models/AccountModel.cs
@@ -4,22 +4,24 @@ namespace kumalo.Models
 {
     public class AccountModel
     {
-        [Required]
+        [Required(ErrorMessage = "First name field is required")]
         public string FirstName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Last name field is required")]
         public string LastName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Age field is required")]
+        [Range(1, 150, ErrorMessage = "Please enter a valid age between 1 and 150")]
         public int Age { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "City name field is required")]
         public string City { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Phone number field is required")]
+        [RegularExpression(@"^[0-9]{10}$", ErrorMessage = "Phone number must contain exactly 10 numeric characters")]
         public string PhoneNumber { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Description field is required")]
         public string Description { get; set; }
     }
 }
